7f4806f7352f1e1b38c1bec4622303c8e74fd176
BONNe <bonne@bonne.id.lv>
2025-09-11 11:22:11 +0300

    Fixes neoforge run

--- neoforge/src/main/java/lv/id/bonne/batzapper/neoforge/client/neoforge/AnimalPenNeoForgeClient.java
+++ neoforge/src/main/java/lv/id/bonne/batzapper/neoforge/client/neoforge/AnimalPenNeoForgeClient.java
@@ -10,7 +10,7 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
 
-@EventBusSubscriber(modid = BatZapper.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = BatZapper.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class AnimalPenNeoForgeClient
 {
     @SubscribeEvent
